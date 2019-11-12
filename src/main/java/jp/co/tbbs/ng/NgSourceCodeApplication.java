8917d6a48069218de25d8e455bd04df6ceca0919
Tsubasa Nemoto <nemoto.tsubasa@gmail.com>
2019-11-12 22:53:46 +0900

    init commit

---
+++ src/main/java/jp/co/tbbs/ng/NgSourceCodeApplication.java
@@ -0,0 +1,13 @@
+package jp.co.tbbs.ng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSourceCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSourceCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/tbbs/ng/NgSourceCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.tbbs.ng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgSourceCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
